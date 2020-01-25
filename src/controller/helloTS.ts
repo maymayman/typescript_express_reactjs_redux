d1867e3729def274d24b36b4abceb578e1bee1a6
phanthong9811 <phanthong.1106@gmail.com>
2020-01-25 16:22:34 +0700

    feat:add login

--- src/controller/helloTS.ts
+++ src/controller/helloTS.ts
@@ -2,7 +2,9 @@ import { Request, Response } from 'express'
 
 export default {
     a : (req :Request ,res:Response) => res.json({code :200}),
-    heloo : (name :String) => {return name+" hello"}
+    login : (req :Request ,res:Response) => {
+        
+    }
 }
 export const sum = (a:number , b :number) => {return a+b}
 // heloo work 
