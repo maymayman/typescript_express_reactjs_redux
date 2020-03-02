a0c29e8f3bc3b8d8158b72b08c71250ba841adda
MACBOOK PRO <a446@446s-MacBook-Pro.local>
2020-03-02 11:38:08 +0700

    fix: fixing stock table

--- migration/up/m-2-Stock.sql
+++ migration/up/m-2-Stock.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS stock (
     id BIGINT NOT NULL AUTO_INCREMENT,
-    stock_name varchar(255) UNIQUE,
+    stock_code varchar(255) UNIQUE ,
+    stock_name varchar(255) NOT NULL,
     stock_price Float(24) NOT NULL,
-    stock_date DATETIME ,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (id)
