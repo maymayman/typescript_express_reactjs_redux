ba09444a9c230b31ec5e4b1650285c9514d483dd
MACBOOK PRO <a446@446s-MacBook-Pro.local>
2020-03-16 16:41:11 +0700

    fix: field token to longtext

--- migration/up/m-3-Session.sql
+++ migration/up/m-3-Session.sql
@@ -1,12 +1,12 @@
 CREATE TABLE IF NOT EXISTS session_users (
     id BIGINT NOT NULL AUTO_INCREMENT,
-    token VARCHAR(255) NOT NULL,
-    users_id BIGINT NOT NULL  ,
+    token LONGTEXT NOT NULL,
+    user_id BIGINT NOT NULL ,
     device VARCHAR(255) ,
     device_id BIGINT ,
     expried_at DATETIME ,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (id),
-    CONSTRAINT fk_table_user FOREIGN KEY (users_id) REFERENCES users(id)
+    CONSTRAINT fk_table_user FOREIGN KEY (user_id) REFERENCES users(id)
 )
