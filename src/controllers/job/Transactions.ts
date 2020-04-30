e2bc372cab03a639f0f043ed79120f5b69aaf285
thongphan9811 <phanthong.1106@gmail.com>
2020-04-30 15:05:15 +0700

    fix: add function urlCrawl

--- src/controllers/job/Transactions.ts
+++ src/controllers/job/Transactions.ts
@@ -3,42 +3,64 @@ import * as rp from 'request-promise';
 import * as Models from '../../models';
 
 const Stocks = Models.default.Stocks;
-const url = {
-  FPT:
-    'https://svr2.fireant.vn/api/Data/Markets/HistoricalQuotes?symbol=FPT&startDate=2020-1-20&endDate=2020-4-19'
+
+interface IUrlCrawl {
+  stockCode: string;
+  startDate: string;
+  endDate: string;
+}
+const urlCrawl = (options: IUrlCrawl): string => {
+  const { startDate, endDate, stockCode } = options;
+  const endpoint =
+    process.env.ENDPOINT_CRAWL_URL ||
+    'https://svr2.fireant.vn/api/Data/Markets/HistoricalQuotes';
+
+  return `${endpoint}/symbol=${stockCode}&startDate=${startDate}&endDate=${endDate};`;
+};
+const crawlByStockCode = async options => {
+  const { element, startDate, endDate } = options;
+  const urlByStockCode = urlCrawl({
+    startDate,
+    endDate,
+    stockCode: element.stock_code
+  });
+  const transactions = await rp({
+    uri: urlByStockCode,
+    headers: {
+      'User-Agent': 'Request-Promise'
+    },
+    json: true
+  });
+  transactions.forEach(async transaction => {
+    await rp({
+      method: 'POST',
+      url: 'http://localhost:3000/api/transactions',
+      body: {
+        stock_id: element.id.toString(),
+        close_price: transaction.Close,
+        open_price: transaction.Open,
+        high_price: transaction.High,
+        low_price: transaction.Low,
+        volume: transaction.Volume,
+        exchange_date: transaction.Date
+      },
+      json: true
+    });
+  });
 };
 
 export default {
   crawl: async (req: Request, res: Response) => {
     const stock = await Stocks.findAll();
+    const startDate = req.query.startDate;
+    const endDate = req.query.endDate;
 
     stock.forEach(async element => {
-      if (url[element.stock_code]) {
-        const transactions = await rp({
-          uri: url[element.stock_code],
-          headers: {
-            'User-Agent': 'Request-Promise'
-          },
-          json: true
-        });
-
-        transactions.forEach(async transaction => {
-          await rp({
-            method: 'POST',
-            url: 'http://localhost:3000/api/transactions',
-            body: {
-              stock_id: element.id.toString(),
-              close_price: transaction.Close,
-              open_price: transaction.Open,
-              high_price: transaction.High,
-              low_price: transaction.Low,
-              volume: transaction.Volume,
-              exchange_date: transaction.Date
-            },
-            json: true
-          });
-        });
-      }
+      await crawlByStockCode({
+        element,
+        startDate,
+        endDate
+      });
     });
 
     return res.json(stock);
