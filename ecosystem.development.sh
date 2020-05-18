1c56ebb76ffabdd0950b3ef906cf10f6812b9d6e
maymayman <tlvduc92@gmail.com>
2020-05-18 16:47:48 +0700

    Update ecosystem.development.sh

--- ecosystem.development.sh
+++ ecosystem.development.sh
@@ -15,7 +15,7 @@ echo "build                            : ===> build project"
 npm run build
 
 echo "migration up                     : ===> migrate database"
-MY_SQL_HOST=localhost MY_SQL_PASSWORD=my-secret-pw MY_SQL_DATABASE_NAME=stock_develop MY_SQL_PORT=3307 npm run migration:up
+MY_SQL_HOST=localhost MY_SQL_PASSWORD=my-secret-pw MY_SQL_DATABASE_NAME=stock_develop MY_SQL_PORT=3306 npm run migration:up
 
 echo "deploy app                       : ===> deploy application"
 pm2 start ecosystem.config.js --env development
